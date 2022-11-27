ec8e2c1f940178ee0ddda97b566651707db792de
ersen <70ozturkersen@gmail.com>
2022-11-27 19:57:35 +0300

    campaigns carried out

--- components/ui/CampaignItem.jsx
+++ components/ui/CampaignItem.jsx
@@ -8,22 +8,21 @@ const CampaignItem = ({ title, discount }) => {
   return (
     <div className="flex flex-1 items-center bg-secondary py-5 px-4 gap-5 rounded-md">
       <div className="relative w-36 h-36  sm:w-40 sm:h-40 ">
-        
-          <Image
-            src="/images/campaignItem1.jpg"
-            alt="campaignItem1.jpg"
-            fill
-            className="border-[5px] border-primary rounded-full hover:scale-105 transition-all delay-100	"
-          />
-        
+        <Image
+          src="/images/campaignItem1.jpg"
+          alt="campaignItem1.jpg"
+          fill
+          className="border-[5px] border-primary rounded-full hover:scale-105 transition-all delay-100	"
+        />
       </div>
       <div className="flex flex-col items-start text-white">
-        <Title titleClass='text-2xl'>{title}</Title>
+        <Title titleClass="text-2xl">{title}</Title>
         <p>
-         <span className="text-[40px] font-dancing" > {discount}%</span> <span className="text-lg font-dancing">off</span>
+          <span className="text-[40px] font-dancing"> {discount}%</span>{" "}
+          <span className="text-lg font-dancing">off</span>
         </p>
-        <PrimayBtn addClass='flex gap-2 items-center'>
-          <span>Order Now</span> <FaShoppingCart size={30}/>
+        <PrimayBtn addClass="flex gap-2 items-center">
+          <span>Order Now</span> <FaShoppingCart size={30} />
         </PrimayBtn>
       </div>
     </div>
