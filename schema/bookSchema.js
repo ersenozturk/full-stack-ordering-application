b52ecd6719e185f5e923178a55c7731670f383c5
ersen <70ozturkersen@gmail.com>
2022-11-28 17:43:35 +0300

    bokkSchema Yp.object re-revised

--- schema/bookSchema.js
+++ schema/bookSchema.js
@@ -5,12 +5,12 @@ export const bookSchema = () =>
     fullName: Yup.string()
       .required("Full name is required.")
       .min(3, "Full name must be at least 3 characters."),
-    phone: Yup.string()
+    phone: Yup.number()
       .required("Full name is required.")
       .min(10, "Phone number must be at least 10 characters."),
     email: Yup.string()
       .required("Email is required.")
       .email("Email is invalid."),
-    manyPerson: Yup.string().required("Persons is required.").min(1,'Booking must be at least 1 people'),
-    date: Yup.string().required("Date is required."),
+    manyPerson: Yup.number().max(100).required().min(1,'Booking must be at least 1 people'),
+    date: Yup.date().default(() => new Date()),
   });
