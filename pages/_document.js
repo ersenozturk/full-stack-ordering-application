d0fa1e7b7b592f3618f3eb6c1bd9f1dae4098817
ersen <70ozturkersen@gmail.com>
2022-11-28 22:08:57 +0300

    fontawesome cdn added

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        {/* fontawesome cdn */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
       <body>
         <Main />
