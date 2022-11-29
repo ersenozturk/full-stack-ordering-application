2f4f27d67ad9bb64e7ea0574ae9ae4186a91ce50
ersen <70ozturkersen@gmail.com>
2022-11-29 12:15:19 +0300

    order page created and order images added

---
+++ pages/order/index.jsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+
+const Order = () => {
+  return (
+    <div className="overflow-x-auto">
+      <div className="min-h-[calc(100vh_-_433px)] min-w-[1000px] flex justify-center items-center flex-col p-10">
+        <div className=" flex items-center flex-1  w-full max-h-28">
+          <table className="w-full text-sm text-center text-gray-500">
+            <thead className="text-xs text-gray-400 uppercase bg-gray-700">
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  ORDER ID
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  CUSTOMER
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  ADDRESS
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  TOTAL
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="transition-all bg-secondary border-gray-700 hover:bg-primary ">
+                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
+                  123abc4x5w...
+                </td>
+                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                  Ersen OZTURK
+                </td>
+                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                  Buca / Izmir
+                </td>
+                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                  $28
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-between w-full p-10 bg-primary mt-6">
+          <div className="relative flex flex-col items-center">
+            <Image
+              src="/images/orderImg/paid.png"
+              alt=""
+              width={40}
+              height={40}
+              objectFit="contain"
+            />
+            <span>Payment</span>
+          </div>
+          <div className="relative flex flex-col items-center animate-pulse">
+            <Image
+              src="/images/orderImg/bake.png"
+              alt=""
+              width={40}
+              height={40}
+              objectFit="contain"
+            />
+            <span>Preparing</span>
+          </div>
+          <div className="relative flex flex-col items-center">
+            <Image
+              src="/images/orderImg/bike.png"
+              alt=""
+              width={40}
+              height={40}
+              objectFit="contain"
+            />
+            <span>On the way</span>
+          </div>
+          <div className="relative flex flex-col items-center">
+            <Image
+              src="/images/orderImg/delivered.png"
+              alt=""
+              width={40}
+              height={40}
+              objectFit="contain"
+            />
+            <span>Delivered</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
