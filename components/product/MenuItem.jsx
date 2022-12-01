28cf50575435608565525d518f3b8286d0be9047
ersen <70ozturkersen@gmail.com>
2022-12-01 15:15:22 +0300

    link added

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FaShoppingCart } from "react-icons/fa";
 
 const MenuItem = () => {
   return (
-    <div className="bg-secondary text-white rounded-3xl">
-      <div className="flex justify-center bg-[#F1F2F3] p-6 rounded-bl-[45px] rounded-tl-2xl rounded-tr-2xl">
-        <div className="relative w-40 h-40 flex ">
-          <Image src="/images/food.png" alt="" fill className="hover:scale-110 transition-transform delay-100" />
+    <div className="bg-secondary text-white rounded-3xl ">
+      <Link href="/product">
+        <div className="flex justify-center bg-[#F1F2F3] p-6 rounded-bl-[45px] rounded-tl-2xl rounded-tr-2xl cursor-pointer">
+          <div className="relative w-40 h-40 flex ">
+            <Image
+              src="/images/food.png"
+              alt=""
+              fill
+              className="hover:scale-110 transition-transform delay-100"
+            />
+          </div>
         </div>
-      </div>
+      </Link>
+
       <div className=" p-6">
         <h2 className="text-xl font-semibold">Delicious Pizza</h2>
         <p className="text-[15px]">
--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,17 +1,77 @@
 import Image from "next/image";
+import { useState } from "react";
 import PrimayBtn from "../../components/ui/PrimayBtn";
 import Title from "../../components/ui/Title";
 
+// const initialProductPriceArr = [30, 51, 73];
+const initialProductPriceArr = [10,11,12];
+const initialExtraArr = [
+  {
+    id: 1,
+    name: "Extra 1",
+    priceExtra: 1,
+  },
+  {
+    id: 2,
+    name: "Extra 2",
+    priceExtra: 2,
+  },
+  {
+    id: 3,
+    name: "Extra 3",
+    priceExtra: 3,
+  },
+];
+
 //! Product page
 const Index = () => {
+  const [priceArr, setPriceArr] = useState(initialProductPriceArr);
+  const [price, setPrice] = useState(priceArr[0]);
+  const [productSize, setProductSize] = useState(0);
+
+  const [extraItem, setExtraItem] = useState(initialExtraArr);
+  const [extras, setExtras] = useState([]);
+
+  //? for product size
+  const handleSize = (element) => {
+    setProductSize(element);
+    const difference = priceArr[element] - priceArr[productSize];
+    changePrice(difference);
+
+    // console.log('element',element);
+    // console.log('productSize',productSize)
+    // console.log('priceArr[element]',priceArr[element]);
+    // console.log('priceArr[productSize]',priceArr[productSize])
+    // console.log(difference);
+  };
+
+  //? for checkbox extra items
+  const handleExtraChange = (e, each) => {
+    if (e.target.checked) {
+      changePrice(each.priceExtra);
+      setExtras([...extras, each]);
+    } else {
+      changePrice(-each.priceExtra);
+      setExtras(extras.filter((clicked) => clicked.id !== each.id));
+    }
+  };
+
+  //? for show price on the screen
+  const changePrice = (elemet) => {
+    setPrice(price + elemet);
+  };
+
   return (
     <div className="flex h-screen items-center gap-20 p-20">
       <div className="relative flex-1 h-[80%] w-[80%]">
         <Image src="/images/food.png" alt="" fill />
       </div>
+
       <div className="flex-1 flex flex-col items-start justify-evenly gap-9">
         <Title titleClass="text-6xl">Good Pizza</Title>
-        <span className="text-primary border-b-[2px] border-primary font-extrabold text-3xl">$19</span>
+        <span className="text-primary border-b-[2px] border-primary font-extrabold text-3xl">
+          $ {price}
+        </span>
         <p className="text-sm">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
           molestiae amet commodi libero corporis, facilis provident praesentium
@@ -21,30 +81,43 @@ const Index = () => {
         <div>
           <h4 className="my-5 font-bold text-lg">Choose the size</h4>
           <div className="flex gap-4">
-            <PrimayBtn addClass="!text-black  font-light text-base px-6 py-2 rounded-xl">
-              Small
-            </PrimayBtn>
-            <PrimayBtn addClass="!text-black  font-medium text-lg px-6 py-2 rounded-xl">
-              Medium
-            </PrimayBtn>
-            <PrimayBtn addClass="!text-black  font-bold text-xl px-6 py-2 rounded-xl">
-              Large
-            </PrimayBtn>
+            <span onClick={() => handleSize(0)}>
+              <PrimayBtn addClass="!text-black  font-light text-base px-6 py-2 rounded-xl">
+                Small
+              </PrimayBtn>
+            </span>
+            <span onClick={() => handleSize(1)}>
+              <PrimayBtn addClass="!text-black  font-medium text-lg px-6 py-2 rounded-xl">
+                Medium
+              </PrimayBtn>
+            </span>
+            <span onClick={() => handleSize(2)}>
+              <PrimayBtn addClass="!text-black  font-bold text-xl px-6 py-2 rounded-xl">
+                Large
+              </PrimayBtn>
+            </span>
           </div>
         </div>
         <div className="flex gap-4 ">
-          <label htmlFor="ketchup" className="flex gap-x-1 items-center">
-            <input type="checkbox" name="ketchup" id="ketchup" className="w-5 h-5 accent-primary" />
-            <span className="font-semibold">ketchup</span>
-          </label>
-          <label htmlFor="mayonnaise" className="flex gap-x-1 items-center">
-            <input type="checkbox" name="mayonnaise" id="mayonnaise" className="w-5 h-5 accent-primary" />
-            <span className="font-semibold">mayonnaise</span>
-          </label>
-          <label htmlFor="mustard" className="flex gap-x-1 items-center">
-            <input type="checkbox" name="mustard" id="mustard" className="w-5 h-5 accent-primary" />
-            <span className="font-semibold">mustard</span>
-          </label>
+          {extraItem.map((each) => {
+            const { id, name, priceExtra } = each;
+            return (
+              <label
+                key={id}
+                htmlFor="ketchup"
+                className="flex gap-x-1 items-center"
+              >
+                <input
+                  type="checkbox"
+                  name={name}
+                  id={id}
+                  className="w-5 h-5 accent-primary"
+                  onChange={(e) => handleExtraChange(e, each)}
+                />
+                <span className="font-semibold">{name}</span>
+              </label>
+            );
+          })}
         </div>
         <PrimayBtn>Add to Cart</PrimayBtn>
       </div>
